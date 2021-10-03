6f334e6ae9781c02d32719876e8b9800f7fd4ac5
M.Ranjbar <amomammadw@gmail.com>
2021-10-03 19:52:23 +0330

    .

--- _nuxt/5dddae4.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{622:function(n,t,e){"use strict";e.r(t);var l={},c=e(59),component=Object(c.a)(l,(function(){var n=this.$createElement,t=this._self._c||n;return t("div",[t("nuxt-child")],1)}),[],!1,null,null,null);t.default=component.exports}}]);
