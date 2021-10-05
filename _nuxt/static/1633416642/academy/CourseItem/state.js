85765897120bbbde577891aa7b6e10875042a35c
M.Ranjbar <amomammadw@gmail.com>
2021-10-05 10:22:38 +0330

    .

---
+++ _nuxt/static/1633416642/academy/CourseItem/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633416642",layout:"default",error:a,state:{counter:0,i18n:{routeParams:{}},auth:{user:a,loggedIn:false,strategy:""}},serverRendered:true,routePath:"\u002Facademy\u002FCourseItem",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"906ca257"}}}}(null));
